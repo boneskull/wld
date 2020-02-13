4ab5abb2bd916a65de978d697bf1610707d1efe8
Christopher Hiller <boneskull@boneskull.com>
2020-02-13 10:15:32 -0800

    remove unused module

--- src/model/aggregate.rs
+++
@@ -1,9 +0,0 @@
-use crate::model::common::TBool;
-
-pub struct TimeStatus<'a> {
-  pub current_time: &'a f64,
-  pub is_daytime: &'a TBool,
-  pub moon_phase: &'a u32,
-  pub sundial_cooldown: &'a u8,
-  pub fast_forward_time: &'a TBool,
-}
