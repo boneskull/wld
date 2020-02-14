6e4f6c380d04f7c17a0e36fc7ee8f643112f438d
Christopher Hiller <boneskull@boneskull.com>
2020-02-13 23:49:11 -0800

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 extern crate wld;
+use wld::parse_world;
 
 fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
-  Ok(())
+  let world: std::vec::Vec<u8> =
+    std::fs::read("tests/fixtures/Foon.wld").expect("Unable to read file");
+
+  match parse_world(&world) {
+    Ok(_) => Ok(()),
+    Err(e) => Err(e),
+  }
 }
