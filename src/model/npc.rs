5aca21c9b46a38eb36c88c0f72f8243b7cca253b
Christopher Hiller <boneskull@boneskull.com>
2020-02-17 10:17:20 -0800

    add TryIntoCtx for npc module; tweak structs

--- src/model/npc.rs
+++ src/model/npc.rs
@@ -1,77 +1,143 @@
 use super::common::*;
 use crate::enums::EntityType;
 use scroll::{
-  ctx::TryFromCtx,
+  ctx::{
+    TryFromCtx,
+    TryIntoCtx,
+  },
+  Endian,
   Error as ScrollError,
   Pread,
+  Pwrite,
   LE,
 };
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct NPC {
-  pub entity: EntityType,
-  pub position: (f32, f32),
+  pub entity_type: EntityType,
+  pub position: Point,
   pub name: TString,
-  pub home_position: Option<Point>,
+  pub home_position: Point,
+  pub is_homeless: TBool,
 }
 
-impl<'a> TryFromCtx<'a, ()> for NPC {
+impl<'a> TryFromCtx<'a, Endian> for NPC {
   type Error = ScrollError;
 
-  fn try_from_ctx(buf: &'a [u8], _: ()) -> Result<(Self, usize), Self::Error> {
+  fn try_from_ctx(
+    buf: &'a [u8],
+    _: Endian,
+  ) -> Result<(Self, usize), Self::Error> {
     let offset = &mut 0;
-    let entity = buf.gread::<EntityType>(offset)?;
+    let entity_type = buf.gread::<EntityType>(offset)?;
     let name = buf.gread::<TString>(offset)?;
-    let position = (
-      buf.gread_with::<f32>(offset, LE)?,
-      buf.gread_with::<f32>(offset, LE)?,
-    );
+    let position = Point {
+      x: buf.gread_with::<f32>(offset, LE)? as i32,
+      y: buf.gread_with::<f32>(offset, LE)? as i32,
+    };
     let is_homeless = buf.gread_with::<TBool>(offset, LE)?;
-    let mut home_position = Some(buf.gread_with::<Point>(offset, LE)?);
-    if is_homeless == TBool::True {
-      home_position = None;
-    }
+    let home_position = buf.gread_with::<Point>(offset, LE)?;
     Ok((
       Self {
-        entity,
+        entity_type,
         position,
         name,
         home_position,
+        is_homeless,
       },
       *offset,
     ))
   }
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct Mob {
-  pub entity: EntityType,
-  pub position: (f32, f32),
-}
-
-impl<'a> TryFromCtx<'a, ()> for Mob {
+impl<'a> TryIntoCtx<Endian> for NPC {
   type Error = ScrollError;
 
-  fn try_from_ctx(buf: &'a [u8], _: ()) -> Result<(Self, usize), Self::Error> {
+  fn try_into_ctx(
+    self,
+    buf: &mut [u8],
+    _: Endian,
+  ) -> Result<usize, Self::Error> {
     let offset = &mut 0;
-    let entity = buf.gread::<EntityType>(offset)?;
-    let position = (
-      buf.gread_with::<f32>(offset, LE)?,
-      buf.gread_with::<f32>(offset, LE)?,
-    );
-    Ok((Self { entity, position }, *offset))
+    let Self {
+      entity_type,
+      name,
+      position,
+      home_position,
+      is_homeless,
+    } = self;
+    buf.gwrite(entity_type, offset)?;
+    buf.gwrite(&name, offset)?;
+    let Point { x, y } = position;
+    buf.gwrite_with(x as f32, offset, LE)?;
+    buf.gwrite_with(y as f32, offset, LE)?;
+    buf.gwrite(&is_homeless, offset)?;
+    buf.gwrite(home_position, offset)?;
+    Ok(*offset)
   }
 }
 
-#[derive(Clone, Debug, Default, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
+pub struct Mob {
+  pub entity_type: EntityType,
+  pub position: Point,
+}
+
+impl<'a> TryFromCtx<'a, Endian> for Mob {
+  type Error = ScrollError;
+
+  fn try_from_ctx(
+    buf: &'a [u8],
+    _: Endian,
+  ) -> Result<(Self, usize), Self::Error> {
+    let offset = &mut 0;
+    let entity_type = buf.gread::<EntityType>(offset)?;
+    let position = Point {
+      x: buf.gread_with::<f32>(offset, LE)? as i32,
+      y: buf.gread_with::<f32>(offset, LE)? as i32,
+    };
+    Ok((
+      Self {
+        entity_type,
+        position,
+      },
+      *offset,
+    ))
+  }
+}
+
+impl TryIntoCtx<Endian> for Mob {
+  type Error = ScrollError;
+
+  fn try_into_ctx(
+    self,
+    buf: &mut [u8],
+    _: Endian,
+  ) -> Result<usize, Self::Error> {
+    let offset = &mut 0;
+    let Self {
+      entity_type,
+      position,
+    } = self;
+    buf.gwrite(entity_type, offset)?;
+    let Point { x, y } = position;
+    buf.gwrite_with(x as f32, offset, LE)?;
+    buf.gwrite_with(y as f32, offset, LE)?;
+    Ok(*offset)
+  }
+}
+#[derive(Clone, Debug, Default, PartialEq, AsRef)]
 pub struct NPCVec(Vec<NPC>);
 
 // todo: investigate a trait to dedupe this TryFromCtx
 
-impl<'a> TryFromCtx<'a, ()> for NPCVec {
+impl<'a> TryFromCtx<'a, Endian> for NPCVec {
   type Error = ScrollError;
 
-  fn try_from_ctx(buf: &'a [u8], _: ()) -> Result<(Self, usize), Self::Error> {
+  fn try_from_ctx(
+    buf: &'a [u8],
+    _: Endian,
+  ) -> Result<(Self, usize), Self::Error> {
     let offset = &mut 0;
     let mut npcs: Vec<NPC> = vec![];
     let mut more_npcs = buf.gread_with::<TBool>(offset, LE)?;
@@ -84,7 +150,35 @@ impl<'a> TryFromCtx<'a, ()> for NPCVec {
   }
 }
 
-#[derive(Clone, Debug, Default, PartialEq)]
+impl TryIntoCtx<Endian> for NPCVec {
+  type Error = ScrollError;
+
+  fn try_into_ctx(
+    self,
+    buf: &mut [u8],
+    _: Endian,
+  ) -> Result<usize, Self::Error> {
+    let offset = &mut 0;
+    let vec = self.as_ref();
+    let len = vec.len();
+    if len > 0 {
+      buf.gwrite(&TBool::True, offset)?;
+    } else {
+      buf.gwrite(&TBool::False, offset)?;
+    }
+    for i in 0..len {
+      buf.gwrite(vec[i].clone(), offset)?;
+      if i == len - 1 {
+        buf.gwrite(&TBool::False, offset)?;
+      } else {
+        buf.gwrite(&TBool::True, offset)?;
+      }
+    }
+    Ok(*offset)
+  }
+}
+
+#[derive(Clone, Debug, Default, PartialEq, AsRef)]
 pub struct MobVec(Vec<Mob>);
 
 impl<'a> TryFromCtx<'a, ()> for MobVec {
@@ -100,5 +194,106 @@ impl<'a> TryFromCtx<'a, ()> for MobVec {
       more_mobs = buf.gread_with::<TBool>(offset, LE)?;
     }
     Ok((Self(mobs), *offset))
+  }
+}
+
+impl TryIntoCtx<Endian> for MobVec {
+  type Error = ScrollError;
+
+  fn try_into_ctx(
+    self,
+    buf: &mut [u8],
+    _: Endian,
+  ) -> Result<usize, Self::Error> {
+    let offset = &mut 0;
+    let vec = self.as_ref();
+    let len = vec.len();
+    if len > 0 {
+      buf.gwrite(&TBool::True, offset)?;
+    } else {
+      buf.gwrite(&TBool::False, offset)?;
+    }
+    for i in 0..len {
+      buf.gwrite(vec[i].clone(), offset)?;
+      if i == len - 1 {
+        buf.gwrite(&TBool::False, offset)?;
+      } else {
+        buf.gwrite(&TBool::True, offset)?;
+      }
+    }
+    Ok(*offset)
+  }
+}
+#[cfg(test)]
+mod test_npc {
+  use super::*;
+
+  #[test]
+  fn test_npc_rw() {
+    let npc = NPC {
+      entity_type: EntityType::TruffleWorm,
+      position: Point { x: 0, y: 0 },
+      name: TString::from("Marvin K. Mooney"),
+      home_position: Point { x: -100, y: -100 },
+      is_homeless: TBool::False,
+    };
+
+    let mut buf = [0; 38];
+    assert_eq!(38, buf.pwrite(npc.clone(), 0).unwrap());
+    assert_eq!(npc, buf.pread::<NPC>(0).unwrap());
+  }
+
+  #[test]
+  fn test_mob_rw() {
+    let mob = Mob {
+      entity_type: EntityType::TruffleWorm,
+      position: Point { x: 0, y: 0 },
+    };
+
+    let mut buf = [0; 12];
+    assert_eq!(12, buf.pwrite(mob.clone(), 0).unwrap());
+    assert_eq!(mob, buf.pread::<Mob>(0).unwrap());
+  }
+
+  #[test]
+  fn test_npc_vec_rw() {
+    let npc_vec = NPCVec(vec![
+      NPC {
+        entity_type: EntityType::TruffleWorm,
+        position: Point { x: 0, y: 0 },
+        name: TString::from("Marvin K. Mooney"),
+        home_position: Point { x: -100, y: -100 },
+        is_homeless: TBool::False,
+      },
+      NPC {
+        entity_type: EntityType::Duck,
+        position: Point { x: 0, y: 0 },
+        name: TString::from("Dave"),
+        home_position: Point { x: -100, y: -100 },
+        is_homeless: TBool::False,
+      },
+    ]);
+
+    let mut buf = [0; 67];
+    assert_eq!(67, buf.pwrite(npc_vec.clone(), 0).unwrap());
+    assert_eq!(npc_vec, buf.pread::<NPCVec>(0).unwrap());
+  }
+
+  #[test]
+  fn test_mob_vec_rw() {
+    let mob_vec = MobVec(vec![
+      Mob {
+        entity_type: EntityType::TruffleWorm,
+        position: Point { x: 0, y: 0 },
+      },
+      Mob {
+        entity_type: EntityType::Duck,
+        position: Point { x: 0, y: 0 },
+      },
+    ]);
+
+    let mut buf = [0; 27];
+    assert_eq!(27, buf.pwrite(mob_vec.clone(), 0).unwrap());
+    assert_eq!(mob_vec, buf.pread::<MobVec>(0).unwrap());
   }
 }
