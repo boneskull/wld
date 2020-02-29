95549394a7efbfb6fe6a18bd2c1c84dd8a8e4682
Christopher Hiller <boneskull@boneskull.com>
2020-02-29 12:39:44 -0800

    move logger init into lib

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@ extern crate scroll_derive;
 #[macro_use]
 extern crate log;
 use crate::model::world::World;
-
+use log::LevelFilter;
+use mowl;
 use std::{
   boxed::Box,
   error::Error,
@@ -22,4 +23,8 @@ pub mod model;
 
 pub fn parse_world<'a>(bytes: &'a [u8]) -> Result<World, Box<dyn Error>> {
   Ok(World::read(bytes)?)
+}
+
+pub fn init_logger() {
+  mowl::init_with_level(LevelFilter::Debug).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-extern crate wld;
-use log::LevelFilter;
-use mowl;
-use wld::parse_world;
+use wld;
+
 fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
-  mowl::init_with_level(LevelFilter::Debug).unwrap();
+  wld::init_logger();
   let world_file: std::vec::Vec<u8> =
     std::fs::read("tests/fixtures/Foon.wld").expect("Unable to read file");
-  let world = parse_world(&world_file)?;
+  let world = wld::parse_world(&world_file)?;
   let rebuilt_world = world.write()?;
   std::fs::write("newworld.wld", rebuilt_world)?;
   Ok(())
